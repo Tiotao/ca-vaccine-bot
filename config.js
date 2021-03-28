0978c08b9b68c65ed6783f79648d1021b5b3df9a
Yiwen Zhang <tiotaocn@gmail.com>
2021-03-28 00:24:50 -0700

    change db

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ const config = {
     AES_KEY: process.env.CA_VACCINE_BOT_AES_KEY || "MY AES KEY",
     WEBHOOK_URL: process.env.BASE_URL + "webhook",
     WEBHOOK_PORT: process.env.PORT || 3000,
-    FETCH_FREQUENCY: "*/60 * * * *",
+    FETCH_FREQUENCY: process.env.CA_VACCINE_BOT_FETCH_FREQUENCY || "*/60 * * * *",
     FETCH_FREQUENCY_DEBUG: "*/1 * * * *",
     VACCINE_API_URL: "https://www.vaccinespotter.org/api/v0/states/CA.json",
 };

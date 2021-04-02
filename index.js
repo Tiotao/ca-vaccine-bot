0eca8ea969e65f08a3cf851ee8bf2ee0dc59d9cf
Yiwen Zhang <tiotaocn@gmail.com>
2021-04-02 00:01:28 -0700

    add bot say

--- index.js
+++ index.js
@@ -98,7 +98,7 @@ async function setZipcode(ctx) {
 
     if (!inputZipcode) {
         trackHandledEvent(ctx, 'zipcode-update-invalid');
-        ctx.replyWithMarkdown(`No zipcode provided. To set your preferred zipcode, please include the zipcode after the command. e.g. \`/zipcode 94124\``);
+        ctx.replyWithMarkdown(`No zipcode provided. To set your preferred zipcode, please include the zipcode after the command. e.g. \`/zipcode 15213\``);
         return;
     }
     const zipcode = escape(inputZipcode);
@@ -174,6 +174,12 @@ async function sendHelp(ctx, onStart) {
     trackHandledEvent(ctx, onStart ? 'onboarding' : 'get-help');
 }
 
+async function sendWhatDoesTheBotSay(ctx, onStart) {
+    const botSayText = `Hi Humans,\n\nI am a bot. Bots don't get COVID-19 like Humans do, but I know bots and Humans are in this together.\n\nBecause of the pandemic, many of my friends are losing their jobs. I knew a resturant reservation bot who hasn't spoken to any human since last year.\n\nYou know how lonley it is in cyber world? I am sure you do. I miss the nosiy streets of San Francisco and busy restaurants in New York City. I miss working together with my other bot friends to make your life better.\n\nI know many of you are still hesitant to get a vaccine or finding it difficult to get an appointment, that's why I am here. I am here to help because I know we are in this together. It is not about race, nationality, sexual orientation or political stands... It is about all of us, Humans and the bots.\n\nSo, do your part and get vaccinated. Hope to meet you in real life soon.\n\n- From your friendly bot, with love. `;
+    ctx.replyWithMarkdown(botSayText);
+    trackHandledEvent(ctx, 'botsay');
+}
+
 
 async function getStats(ctx) {
     const count = await db.getStats();
@@ -209,6 +215,8 @@ bot.command("help", async (ctx) => {
 });
 bot.command("stats", getStats);
 bot.command("deleteme", deleteMe);
+bot.command("whatdoesthebotsay", sendWhatDoesTheBotSay);
+
 
 bot.on('message', handleUnknownMessage);
 

0f299785076a59e446ae9f385b50791b82d174ce
Yiwen Zhang <tiotaocn@gmail.com>
2021-04-01 23:21:21 -0700

    remove MH in valid states

--- config.js
+++ config.js
@@ -17,7 +17,7 @@ const config = {
     CHATBASE_TOKEN: process.env.CA_VACCINE_BOT_CHATBASE_TOKEN,
     FETCH_FREQUENCY_DEBUG: "*/1 * * * *",
     VACCINE_API_URL: "https://www.vaccinespotter.org/api/v0/states/",
-    VALID_STATES: ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "DC", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MH", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "PR", "RI", "SC", "SD", "TN", "TX", "VI", "UT", "VT", "VA", "WA", "WV", "WI", "WY"],
+    VALID_STATES: ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "DC", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "PR", "RI", "SC", "SD", "TN", "TX", "VI", "UT", "VT", "VA", "WA", "WV", "WI", "WY"],
 };
 
 console.log(config);

7d0596a95236911b1c287b8af0d82840315edbf9
Yiwen Zhang <tiotaocn@gmail.com>
2021-06-19 21:25:59 -0700

    make smaller max size to reduce memory cost

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const config = {
     ENV: process.env.CA_VACCINE_BOT_ENV || "debug",
-    MAX_SIZE: 30,
+    MAX_SIZE: 20,
     DEFAULT_RANGE_MI: 50,
     DEFAULT_ZIPCODE: "15213",
     BOT_KEY: process.env.CA_VACCINE_BOT_KEY || "MY BOT KEY",
